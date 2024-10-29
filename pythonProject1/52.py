9db254f970de7c07e9aa2d9b13d0deb61484e71c
Sigwer <polkatelka228@gmail.com>
2024-10-29 22:40:53 +0300

    OAP
    DZ

---
+++ pythonProject1/52.py
@@ -0,0 +1,8 @@
+from unittest import registerResult
+
+x1=float(input("Введите цену хлеба"))
+x2=float(input("Введите цену сахара"))
+x3=float(input("Введите цену яблок"))
+N=int(input("Введите количество яблок"))
+Result=(2*x1)+(1*x2)+(N*x3)
+print(Result)
